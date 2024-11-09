ec991856ed094e400c2713205ae814b9938606a5
hacksparr0w <hacksparr0w@protonmail.com>
2024-11-09 09:47:51 +0100

    chore(test): more tests

--- tests/test_kats.py
+++ tests/test_kats.py
@@ -41,6 +41,181 @@ import pytest
             "output_length": 32,
             "type": argon2.Argon2Type.D,
             "version": argon2.Argon2Version.V10
+        },
+        {
+            "password": (
+                """
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                """
+            ),
+            "salt": (
+                """
+                02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02
+                """
+            ),
+            "secret": (
+                """
+                03 03 03 03 03 03 03 03
+                """
+            ),
+            "ad": (
+                """
+                04 04 04 04 04 04 04 04 04 04 04 04
+                """
+            ),
+            "hash": (
+                """
+                87 ae ed d6 51 7a b8 30 cd 97 65 cd 82 31 ab b2
+                e6 47 a5 de e0 8f 7c 05 e0 2f cb 76 33 35 d0 fd
+                """
+            ),
+            "memory": 32,
+            "iterations": 3,
+            "parallelism": 4,
+            "output_length": 32,
+            "type": argon2.Argon2Type.I,
+            "version": argon2.Argon2Version.V10
+        },
+        {
+            "password": (
+                """
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                """
+            ),
+            "salt": (
+                """
+                02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02
+                """
+            ),
+            "secret": (
+                """
+                03 03 03 03 03 03 03 03
+                """
+            ),
+            "ad": (
+                """
+                04 04 04 04 04 04 04 04 04 04 04 04
+                """
+            ),
+            "hash": (
+                """
+                b6 46 15 f0 77 89 b6 6b 64 5b 67 ee 9e d3 b3 77
+                ae 35 0b 6b fc bb 0f c9 51 41 ea 8f 32 26 13 c0
+                """
+            ),
+            "memory": 32,
+            "iterations": 3,
+            "parallelism": 4,
+            "output_length": 32,
+            "type": argon2.Argon2Type.ID,
+            "version": argon2.Argon2Version.V10
+        },
+        {
+            "password": (
+                """
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                """
+            ),
+            "salt": (
+                """
+                02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02
+                """
+            ),
+            "secret": (
+                """
+                03 03 03 03 03 03 03 03
+                """
+            ),
+            "ad": (
+                """
+                04 04 04 04 04 04 04 04 04 04 04 04
+                """
+            ),
+            "hash": (
+                """
+                51 2b 39 1b 6f 11 62 97 53 71 d3 09 19 73 42 94
+                f8 68 e3 be 39 84 f3 c1 a1 3a 4d b9 fa be 4a cb
+                """
+            ),
+            "memory": 32,
+            "iterations": 3,
+            "parallelism": 4,
+            "output_length": 32,
+            "type": argon2.Argon2Type.D,
+            "version": argon2.Argon2Version.V13
+        },
+        {
+            "password": (
+                """
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                """
+            ),
+            "salt": (
+                """
+                02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02
+                """
+            ),
+            "secret": (
+                """
+                03 03 03 03 03 03 03 03
+                """
+            ),
+            "ad": (
+                """
+                04 04 04 04 04 04 04 04 04 04 04 04
+                """
+            ),
+            "hash": (
+                """
+                c8 14 d9 d1 dc 7f 37 aa 13 f0 d7 7f 24 94 bd a1
+                c8 de 6b 01 6d d3 88 d2 99 52 a4 c4 67 2b 6c e8
+                """
+            ),
+            "memory": 32,
+            "iterations": 3,
+            "parallelism": 4,
+            "output_length": 32,
+            "type": argon2.Argon2Type.I,
+            "version": argon2.Argon2Version.V13
+        },
+        {
+            "password": (
+                """
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                """
+            ),
+            "salt": (
+                """
+                02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02
+                """
+            ),
+            "secret": (
+                """
+                03 03 03 03 03 03 03 03
+                """
+            ),
+            "ad": (
+                """
+                04 04 04 04 04 04 04 04 04 04 04 04
+                """
+            ),
+            "hash": (
+                """
+                0d 64 0d f5 8d 78 76 6c 08 c0 37 a3 4a 8b 53 c9
+                d0 1e f0 45 2d 75 b6 5e b5 25 20 e9 6b 01 e6 59
+                """
+            ),
+            "memory": 32,
+            "iterations": 3,
+            "parallelism": 4,
+            "output_length": 32,
+            "type": argon2.Argon2Type.ID,
+            "version": argon2.Argon2Version.V13
         }
     ]
 )
